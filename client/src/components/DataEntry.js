95f758d38ea4a22eca62b7147cd7bd644d12e8e0
David Roussov <david.roussov@hotmail.com>
2017-08-07 00:03:07 +1000

    front end for edit/delete maptime category

--- client/src/components/DataEntry.js
+++ client/src/components/DataEntry.js
@@ -2,12 +2,13 @@ import React, { Component } from  "react";
 import { connect } from 'react-redux';
 
 import { createNewValue } from '../actions/mapTime';
+import EditMapTimeCategory from './EditMapTimeCategory';
 
 class DataEntry extends Component {
 
     constructor() {
       super();
-      this.state = {'newValue': ''};
+      this.state = {'newValue': '', 'showTopicModal': false};
     }
 
     createNewValue(e) {
@@ -35,6 +36,16 @@ class DataEntry extends Component {
         return `${days[date.getDay()]}
                 ${date.getUTCFullYear()}/${date.getUTCMonth()}/${date.getUTCDate()}
                 ${date.getHours()}:${date.getMinutes()}`;
+    }
+
+    toggleHover = () => this.setState({ hoverHeading: !this.state.hoverHeading });
+
+    toggleCategoryModal() {
+      if (this.state.hoverHeading) {
+        this.setState({ showTopicModal: true });
+      } else {
+        this.setState({ showTopicModal: false });
+      }
     }
 
     render() {
@@ -68,24 +79,31 @@ class DataEntry extends Component {
           </tr>
         );
 
+        if(this.state.hoverHeading) {
+          style.heading['textDecoration'] = 'underline';
+          style.heading['cursor'] = 'pointer';
+        }
+
         return (
             <div style={style.container}>
-                <h2 style={style.heading}>{this.props.currentMapping}</h2>
-                <form onSubmit={this.createNewValue.bind(this)} style={style.inputContainer}>
-                    <input type="text" className="form-control" value={this.state.newValue} onChange={this.handleInputValue.bind(this)}/>
-                    <button type="submit" className="btn btn-success" style={style.submitButton}>Submit</button>   
-                </form>
-                <table style={style.table} className="table table-bordered table-hover">
-                  <thead>
-                    <tr className="warning">    
-                      <th>Time</th>
-                      <th>Value</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tableOfPast}
-                  </tbody>
-                </table>
+              <h2 style={style.heading} onClick={this.toggleCategoryModal.bind(this)} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>{this.props.currentMapping}</h2>
+              <form onSubmit={this.createNewValue.bind(this)} style={style.inputContainer}>
+                  <input type="text" className="form-control" value={this.state.newValue} onChange={this.handleInputValue.bind(this)}/>
+                  <button type="submit" className="btn btn-success" style={style.submitButton}>Submit</button>   
+              </form>
+              <table style={style.table} className="table table-bordered table-hover">
+                <thead>
+                  <tr className="warning">    
+                    <th>Time</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableOfPast}
+                </tbody>
+              </table>
+
+              { this.state.showTopicModal ? <EditMapTimeCategory toggleShow={this.toggleCategoryModal.bind(this)}/> : null }; 
             </div>
         )
     } 
